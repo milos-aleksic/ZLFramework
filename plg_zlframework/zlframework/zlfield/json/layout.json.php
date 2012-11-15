217af48f1553f14bf395282ea485224174ed4a89
Miljan <miljan@aleksic.es>
2012-11-15 20:24:37 +0800

    ZL Field layout.json Default value fixed & code cleanup

--- plg_zlframework/zlframework/zlfield/json/layout.json.php
+++ plg_zlframework/zlframework/zlfield/json/layout.json.php
@@ -9,23 +9,33 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
+	// init vars
+	$mode = $params->find('layout.mode', 'files');
+	$path = $params->find('layout.path');
+	$regex = $params->find('layout.regex');
+
+	// set default regex
+	if(empty($regex)){
+		$regex = $mode == 'files' ? '^([^_][_A-Za-z0-9]*)\.php$' : '';
+	}
+
+	// set static layouts
 	$static_main_layouts = array();
 	foreach ($params->find('layout.options', array()) as $name => $value){
 		$static_main_layouts[$name] = $value;
 	}
 
 	// set default
-	$default = $params->find('layout.default', 'default.php');
-	$default = $params->find('layout.mode') == 'folders' ? 'default' : $default;
+	$default = $params->find('layout.default', '');
+	if(empty($default)){
+		$func = $mode == 'files' ? 'files' : 'dirs';
+		$default = array_shift($this->app->path->$func($path, false, "/$regex/"));
+	}
 
-	// create path to params
-	// $path 	  = (string)$node->attributes()->path_overrided ? (string)$node->attributes()->path_overrided : (string)$node->attributes()->path;
+	// encode path
+	$path = json_encode($path);
 
-	// init vars
-	$path = json_encode($params->find('layout.path'));
-
-	/* regext - ^([^_][_A-Za-z0-9]*)\.php$ - will show only php files that don't start with "_" */
-
+	// json
 	return
 	'{
 		"_layout":{
@@ -34,9 +44,9 @@ defined('_JEXEC') or die('Restricted access');
 			"help":"'.$params->find('layout.help').'",
 			"default":"'.$default.'",
 			"specific":{
-				"mode":"'.$params->find('layout.mode', 'files').'",
+				"mode":"'.$mode.'",
 				"path":'.$path.',
-				"regex":'.json_encode($params->find('layout.mode') == 'folders' ? '' : $params->find('layout.regex', '^([^_][_A-Za-z0-9]*)\.php$')).', 
+				"regex":'.json_encode($regex).', 
 				"options":'.json_encode($static_main_layouts).'
 			},
 			"childs":{
